df0afa60e8eaaed0cf74bb1813dba3ff2fde9bab
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-12-10 22:15:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,11 +9,11 @@ class Triangle
   end
 
   def kind
-    if @s1==@s2 && @s2==@s3 && @s1 == @s3
+    if @s1==@s2 && @s2==@s3 && @s1==@s3
       return :equilateral
-    elsif @s1==@s2 || @s2==@s3 || @s1 == @s3
+    elsif @s1==@s2 || @s2==@s3 || @s1==@s3
       :isosceles
-    elsif @s1!=@s2 && @s2!=@s3 && @s1 != @s3
+    elsif @s1!=@s2 && @s2!=@s3 && @s1!=@s3
       :isosceles
     end
   end
