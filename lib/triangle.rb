83ff9d358eee84d8b1be7bababa655c6b83305ac
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-12-11 03:26:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,21 +1,28 @@
 class Triangle
-  def initialize(*args)
-    @sides = *args.sort
+  # write code here
+  attr_accessor :kind
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
   end
 
   def kind
-    raise TriangleError if @sides.any? {|s| s <= 0} or
-      @sides[0]+ @sides[1] <= @sides[2]
-    case @sides.uniq.size
-    when 1
-      :equilateral
-    when 2
+    if @s1==@s2 && @s2==@s3 && @s1==@s3
+      return :equilateral
+    elsif @s1==@s2 || @s2==@s3 || @s1==@s3
       :isosceles
-    else
+    elsif @s1!=@s2 && @s2!=@s3 && @s1!=@s3
       :scalene
+    elsif @s1 == 0 || @s2 == 0 || @s3 == 0
+        raise TriangleError
     end
   end
-end
 
-class TriangleError < StandardError
+  class TriangleError < StandardError
+    def message
+      "Invalid Triangle"
+    end
+  end
 end
