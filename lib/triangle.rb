6b266305125412b4c456afed8882a588eb156e93
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-12-11 03:29:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,7 +15,7 @@ class Triangle
       :isosceles
     elsif @s1!=@s2 && @s2!=@s3 && @s1!=@s3
       :scalene
-    elsif @s1 + @s2 == @s3
+    elsif @s1 + @s2 > @s3
         raise TriangleError
     end
   end
