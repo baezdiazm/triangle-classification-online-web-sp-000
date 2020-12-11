58deef3380107244920f5cd01818b51119494c93
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2020-12-11 03:25:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,45 +1,28 @@
 class Triangle
-  def initialize(side_1, side_2, side_3)
-    @triangle_sides = []
-    @triangle_sides << side_1
-    @triangle_sides << side_2
-    @triangle_sides << side_3
-  end
+  # write code here
+  attr_accessor :kind
 
-  def valid?
-    sum_one_two = @triangle_sides[0] + @triangle_sides[1]
-    sum_one_three = @triangle_sides[0] + @triangle_sides[2]
-    sum_two_three = @triangle_sides[1] + @triangle_sides[2]
-
-    if (@triangle_sides.none? {|side| side <= 0}) &&
-      (sum_one_two > @triangle_sides[2] && sum_one_three > @triangle_sides[1] && sum_two_three > @triangle_sides[0])
-      return true
-    else
-      return false
-    end
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
   end
 
   def kind
-    if valid?
-      if @triangle_sides.uniq.length == 1
-        return :equilateral
-      elsif @triangle_sides.uniq.length == 2
-        return :isosceles
-      else
-        return :scalene
-      end
-    else
-      begin
+    if @s1==@s2 && @s2==@s3 && @s1==@s3
+      return :equilateral
+    elsif @s1==@s2 || @s2==@s3 || @s1==@s3
+      :isosceles
+    elsif @s1!=@s2 && @s2!=@s3 && @s1!=@s3
+      :scalene
+    elsif @s1 == 0 || @s2 == 0 || @s3 == 0
         raise TriangleError
-      rescue  TriangleError => error
-        puts error.message
-      end
     end
   end
-end
 
-class TriangleError < StandardError
-  def message
-    "OOF"
+  class TriangleError < StandardError
+    def message
+      "Invalid Triangle"
+    end
   end
 end
